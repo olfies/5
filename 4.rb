c58240f7d6949adadf4edac8136cf8450326458b
olfies <119305253+olfies@users.noreply.github.com>
2022-12-08 20:31:16 +0300

    Add files via upload

---
+++ 4.rb
@@ -0,0 +1,17 @@
+puts "Введите 3 коффицента"
+a = gets.chomp.to_f
+b = gets.chomp.to_f
+c = gets.chomp.to_f
+def result (a,b,c)
+    dis = b**2-4*a*c
+if dis < 0
+     "Дискриминант #{dis}.Корней нет"
+elsif
+    dis == 0 
+     "Дискриминант #{dis}.Корни равны"
+else  
+    dis > 0  
+     "Дискриминант #{dis}."#{-b - Math.sqrt(dis)/a*2} и "#{-b + Math.sqrt(dis)/a*2}"
+    end 
+    end
+puts result(a,b,c)
