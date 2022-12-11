0508d3331bf7f63b19de4c97ef335c199ffa16a0
olfies <119305253+olfies@users.noreply.github.com>
2022-12-11 15:15:31 +0300

    Add files via upload
    По моему мнению я исправил всё то что вы отметили , если тут опять что-то не так , то прошу обьяснить что именно

--- 1.rb
+++ 1.rb
@@ -6,7 +6,7 @@ puts "#{name}, какой у тебя рост?"
 height = gets.chomp
 
 if (height.to_i - 110)>0
-puts "#{name}, твой идеальный вес #{height.to_i - 110}"
-else 
-puts "Ваш вес уже оптимальный"
+    puts "#{name}, твой идеальный вес #{height.to_i - 110}"
+else
+    puts "Ваш вес уже оптимальный"
 end
--- 4.rb
+++ 4.rb
@@ -3,15 +3,15 @@ a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
 def result (a,b,c)
-    dis = b**2-4*a*c
+    dis = b**2 - 4 * a * c
 if dis < 0
      "Дискриминант #{dis}.Корней нет"
 elsif
-    dis == 0 
-     "Дискриминант #{dis}.Корни равны"
-else  
-    dis > 0  
-     "Дискриминант #{dis}."#{-b - Math.sqrt(dis)/a*2} и "#{-b + Math.sqrt(dis)/a*2}"
+   dis == 0
+    "Дискриминант #{dis}.Корни равны"
+else
+   dis > 0
+    "Дискриминант #{dis}."#{-b - Math.sqrt(dis)/a * 2} и "#{-b + Math.sqrt(dis)/a * 2}"
     end 
     end
 puts result(a,b,c)
