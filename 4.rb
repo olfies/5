c60bf9e7662134ea248885142028e040af7e4132
olfies <119305253+olfies@users.noreply.github.com>
2022-12-14 19:20:24 +0300

    Update 4.rb

--- 4.rb
+++ 4.rb
@@ -2,7 +2,7 @@ puts "Введите 3 коффицента"
 a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
-def result (a,b,c)
+def result (a, b, c)
   dis = b**2 - 4 * a * c
 if dis < 0
   "Дискриминант #{dis}.Корней нет"
@@ -10,6 +10,6 @@ elsif dis == 0
   "Дискриминант #{dis}.Корни равны"
 else
   "Дискриминант #{dis}. #{-b - Math.sqrt(dis)/a * 2} и #{-b + Math.sqrt(dis)/a * 2}"
+  end
 end
-end
-puts result(a,b,c)
+puts result(a, b, c)
